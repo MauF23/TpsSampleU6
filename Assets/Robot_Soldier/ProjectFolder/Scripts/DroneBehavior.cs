13e6d7986a7d650591b1f1f99160e47351b4de09
MauF23 <mauriciofuentesw23e@gmail.com>
2025-04-10 14:01:16 -0600

    Clase Tweens

--- Assets/Robot_Soldier/ProjectFolder/Scripts/DroneBehavior.cs
+++ Assets/Robot_Soldier/ProjectFolder/Scripts/DroneBehavior.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
-using DG.Tweening;
+using DG.Tweening; //Importar DOTWEEN al script
 
 public class DroneBehavior : MonoBehaviour
 {
     public Transform[] waypoints;
-    private int currentWayPointIndex;
-    private Transform currentWayPoint;
+    public int currentWayPointIndex = 0;
+    public Transform currentWayPoint;
 
     public float rotateTime, movementTime;
 
     Sequence sequenceMovement;
+
+    //Suavizado de la rotación.
+    public Ease rotateEase;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,10 +30,22 @@ public class DroneBehavior : MonoBehaviour
             currentWayPointIndex = 0;
         }
         currentWayPoint = waypoints[currentWayPointIndex];
+        Movement();
     }
     public void Movement()
     {
+        sequenceMovement = DOTween.Sequence(); //inicializar la secuencia.
 
+        //Append añade tweens a la secuencia de forma secuencial, es decir se ejecuta cuando TERMINA el anterior.
+        sequenceMovement.Append(transform.DOLookAt(currentWayPoint.position, rotateTime).SetEase(rotateEase));
+
+        //Join añade tweens a la secuencia de forma paralela, es decir se ejecuta AL MISMO TIEMPO que el anterior.
+        sequenceMovement.Join(transform.DOMove(currentWayPoint.position, movementTime));
+
+        sequenceMovement.Play();
+
+        //Obtener el siguiente waypoint al terminar la secuencia.
+        sequenceMovement.OnComplete(GetNextWaypoint);
     }
 
     public void ResumeMovement()
